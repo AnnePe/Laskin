e0480751298a2c7dfe59b7e33907cb3e2c92067e
AnnePe <anne.pelin@koodata.fi>
2022-09-02 12:15:14 +0300

    muokattu button style

--- App.js
+++ App.js
@@ -1,48 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { StyleSheet, Text, View, TextInput, Alert, Button, Image} from 'react-native';
 
 export default function App() {
   const [number1, setNumber1] = useState(0);
   const [number2, setNumber2] = useState(0);
+  
 
   const buttonPressedPlus = () => { 
      Alert.alert('Result: ' + (parseInt(number1) + parseInt(number2))); 
+     setNumber1(0);
+     setNumber2(0);
+    
   };
   const buttonPressedMinus = () => { 
     
     Alert.alert('Result: ' + (parseInt(number1) - parseInt(number2))); 
+    setNumber1(0);
+    setNumber2(0);
+    
   };
   return (
     
     <View style={styles.container}>
-         <Image style={styles.image} source={{uri: 'https://www.sttinfo.fi/data/images/00304/be7db042-6b61-49f9-9bcd-7fd41b7bc35d.jpg'}} />
+        <Image style={styles.image} source={{uri: 'https://www.sttinfo.fi/data/images/00304/be7db042-6b61-49f9-9bcd-7fd41b7bc35d.jpg'}} />
         <TextInput placeholder='Anna numero1' keyboardType="numeric" style={styles.input} onChangeText={number1 => setNumber1(number1)} value={number1}/>
-         <TextInput placeholder='Anna numero2' keyboardType="numeric" style={styles.input} onChangeText={number2 => setNumber2(number2)} value={number2}/>
-      <View style={styles.button}>
-        <Button onPress={buttonPressedPlus} title="+" />
-        <Button onPress={buttonPressedMinus} title="-" />
+        <TextInput placeholder='Anna numero2' keyboardType="numeric" style={styles.input} onChangeText={number2 => setNumber2(number2)} value={number2}/>
+      <View style={styles.button2}>
+          <View style={styles.button}>
+             <Button onPress={buttonPressedPlus} title="+" />
+          </View>
+          <View style={styles.button}>
+            <Button onPress={buttonPressedMinus} title="-" />
+          </View>
       </View>
-      <StatusBar style="auto" />
+          
+        <StatusBar style="auto" />
     </View>
+      
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    flexDirection:'column',
     alignItems: 'center',
-    justifyContent: 'space-around',
+    justifyContent: 'center',
     backgroundColor: '#fff',
   },
   button: {
-    flex:1,
-    flexDirection:'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    
+    borderWidth: 1,
+    margin:10,
+   
     },
+    button2: {
+      flexDirection:'row',
+      justifyContent: 'space-evenly',
+      
+      },
   image : {
     marginTop:30,
     marginBottom:30,
@@ -50,10 +67,11 @@ const styles = StyleSheet.create({
     height: 100
   },
   input : {
-    width:200  , 
+    width:'50%'  , 
     borderColor: 'gray', 
     borderWidth: 1,
     borderRadius:10,
-    padding: 10,
+    padding: 5,
+    margin:10,
   }
 });
